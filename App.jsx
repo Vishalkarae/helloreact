dcc8ffd230d4306f6ed7b57a0b4215eb8ebbe7c3
vishalkarae <vishalvish1810@gmail.com>
2023-07-13 16:07:00 +0530

    app file changes

---
+++ App.jsx
@@ -0,0 +1,3 @@
+const div=React.createElement("div",{},React.createElement("h1",{className:"hello"},"Hello Vishal from React"))
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(div)
